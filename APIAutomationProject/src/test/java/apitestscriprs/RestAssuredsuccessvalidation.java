8b0dc38da6f01e064d7a055b7a5135c4b509254d
Mukti Garg <muktisdet@gmail.com>
2025-05-22 11:40:23 +0530

    second commit

--- APIAutomationProject/src/test/java/apitestscriprs/RestAssuredsuccessvalidation.java
+++ APIAutomationProject/src/test/java/apitestscriprs/RestAssuredsuccessvalidation.java
@@ -3,6 +3,8 @@ import io.restassured.*;
 public class RestAssuredsuccessvalidation {
 
 	public static void main(String[] args) {
+		
+		System.out.println("this is rest assured api automation");
 	//	RestAssured
 	}
 }
